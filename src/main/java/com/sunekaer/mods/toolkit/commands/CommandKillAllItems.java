49b0d743414f7913433c77faee2c6d3d3ca85107
Sunekaer <kaspar@sunekaer.com>
2019-12-15 01:12:19 +0100

    Added new commands (killAll, killAllAnimals, killAllExpOrbs, killAllItems, killAllMonster, Slayer(gives a sword with sharpness 32767))

---
+++ src/main/java/com/sunekaer/mods/toolkit/commands/CommandKillAllItems.java
@@ -0,0 +1,51 @@
+package com.sunekaer.mods.toolkit.commands;
+
+
+import com.mojang.brigadier.builder.ArgumentBuilder;
+import net.minecraft.command.CommandSource;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static net.minecraft.command.Commands.literal;
+
+public class CommandKillAllItems {
+    public static ArgumentBuilder<CommandSource, ?> register() {
+        return literal("killAllItems")
+                .requires(cs -> cs.hasPermissionLevel(2))
+                        .executes(ctx -> remove(
+                                ctx.getSource()
+                        )
+                );
+    }
+
+    private static int remove(CommandSource source) {
+        World world = source.getWorld();
+        AtomicInteger i = new AtomicInteger();
+
+        source.sendFeedback(new TranslationTextComponent("commands.toolkit.killall.items.start"), true);
+        Stream<Entity> stream = ((ServerWorld) world).getEntities();
+
+        stream.collect(Collectors.toList()).forEach(entity -> {
+            System.out.println(entity);
+            if ((entity instanceof ItemEntity)){
+                System.out.println(entity);
+                i.addAndGet(1);
+                entity.onKillCommand();
+            }
+        });
+        if (i.get() > 0) {
+            source.sendFeedback(new TranslationTextComponent("commands.toolkit.killall.items.done", i.get()), true);
+        } else {
+            source.sendFeedback(new TranslationTextComponent("commands.toolkit.killall.items.no"), true);
+        }
+        return 1;
+    }
+}
+
--- src/main/java/com/sunekaer/mods/toolkit/commands/TKCommand.java
+++ src/main/java/com/sunekaer/mods/toolkit/commands/TKCommand.java
@@ -4,20 +4,23 @@ import com.mojang.brigadier.CommandDispatcher;
 import net.minecraft.command.CommandSource;
 import net.minecraft.command.Commands;
 
-
 public class TKCommand {
-    public TKCommand(CommandDispatcher<CommandSource> dispatcher)
-    {
+    public TKCommand(CommandDispatcher<CommandSource> dispatcher) {
         dispatcher.register(
-                Commands.literal("tk")
+            Commands.literal("tk")
                 .then(CommandDevEnv.register())
                 .then(CommandHand.register())
                 .then(CommandHotbar.register())
                 .then(CommandInventory.register())
-                //.then(CommandLookingAt.register())
+                .then(CommandSlayer.register())
                 .then(CommandOreDist.register())
                 .then(CommandClear.register())
                 .then(CommandStructureClean.register())
+                .then(CommandKillAll.register())
+                .then(CommandKillAllMonsters.register())
+                .then(CommandKillAllAnimals.register())
+                .then(CommandKillAllItems.register())
+                .then(CommandKillAllExpOrbs.register())
         );
     }
 }
